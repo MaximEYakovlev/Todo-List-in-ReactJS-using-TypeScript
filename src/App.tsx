0800ac6ee71897a50f758382da4d3ddc289e9370
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2021-12-31 15:08:15 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, ChangeEvent, useState } from "react";
+import "./App.css";
+import { ITask } from "./Interfaces";
+import { TodoTask } from "./Components/TodoTask";
+
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadLine, setDeadLine] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadLine(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadLine: deadLine };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDeadLine(0);
+  };
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName !== taskNameToDelete;
+      })
+    );
+  };
 
 function App() {
   return (
